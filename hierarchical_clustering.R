f48eb93ede3f6ad3e34a1c9dd7457af47301121c
juan190199 <juanmanueldasilvatome@gmail.com>
2023-02-06 23:45:59 +0100

    Calculating pairwise distances between clusters

--- hierarchical_clustering.R
+++ hierarchical_clustering.R
@@ -16,6 +16,19 @@ euclidean_distance <- function(x, y) {
   sqrt(sum((x - y)^2))
 }
 
-
+# Main function to perform Agglomerative Hierarchical Clustering
+agglomerative_hierarchical_clustering <- function(X, linkage_fun) {
+  n <- nrow(X)  # Number of datapoints
+  clusters <- list(1:n)
+  cluster_distances <- matrix(0, nrow=n, ncol=n)
+  while (length(clusters) > 1) {
+    # Calculate pairwise distances between clusters
+    for (i in 1:(length(clusters) - 1)) {
+      for (j in (i + 1):length(clusters)) {
+        cluster_distances[i, j] <- linkage_fun(clusters[[i]], clusters[[j]])
+      }
+    }
+  }
+}
 
 
