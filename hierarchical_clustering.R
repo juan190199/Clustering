29d9211e033bc55e2ea19fa3bcc3ba42c69581c3
juan190199 <juanmanueldasilvatome@gmail.com>
2023-02-09 23:36:50 +0100

    ~// For each cluster set the distance within cluster members to infinity

--- hierarchical_clustering.R
+++ hierarchical_clustering.R
@@ -33,7 +33,7 @@ agglomerative_hierarchical_clustering <- function(X, linkage_fun) {
   diag(cluster_distances) <- rep(1e10, n)  # Set diagonal to high value to avoid self-pairing
 
   # Initialize each data point as a single-point cluster
-  clusters <- c(1:n)
+  clusters <- as.list(1:n)
 
   # Define function for linkage method
   linkage_fun <- switch(
@@ -75,22 +75,33 @@ agglomerative_hierarchical_clustering <- function(X, linkage_fun) {
 
   while (length(clusters) > 1) {
     # Calculate pairwise distances between clusters
-    for (i in 1:(length(clusters) - 1)) {
-      for (j in (i + 1):length(clusters)) {
+    for (i in 1:(length(clusters) - 1))
+    {
+      for (j in (i + 1):length(clusters))
+      {
         cluster_distances[i, j] <- linkage_fun(X[clusters[[i]], , drop=FALSE], X[clusters[[j]], , drop=FALSE])
       }
     }
 
     # Find the two closest clusters to merge
     min_distance <- min(cluster_distances[cluster_distances > 0])
-    close_cluster_indices <- which(cluster_distances == min_distance, arr.ind = TRUE)
-    close_cluster1 <- close_cluster_indices[1, 1]
-    close_cluster2 <- close_cluster_indices[1, 2]
+    close_cluster_indices <- which(cluster_distances == min_distance, arr.ind=TRUE)
+    close_cluster1 <- close_cluster_indices[[1]]
+    close_cluster2 <- close_cluster_indices[[2]]
 
     # Merge the two closest clusters
     clusters[[close_cluster1]] <- c(clusters[[close_cluster1]], clusters[[close_cluster2]])
     clusters <- clusters[-close_cluster2]
-    cluster_distances <- cluster_distances[-close_cluster2, -close_cluster2]
+    # cluster_distances <- cluster_distances[-close_cluster2, -close_cluster2]
+
+    # For each cluster set the distance between elements of the same cluster to inf
+    for (i in 1:(length(clusters[[close_cluster1]])))
+    {
+      for (j in 1:(length(clusters[[close_cluster2]])))
+      {
+        cluster_distances[close_cluster1[i], close_cluster2[j]] <- 1e10
+      }
+    }
   }
 
   return(clusters[[1]])
