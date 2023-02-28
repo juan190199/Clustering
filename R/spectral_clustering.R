b0ef8e560a84c3ed8888ef1f25690c81a1d5af28
amrei <amrei@example.com>
2023-02-28 23:53:49 +0100

    first version of kernel function

---
+++ R/spectral_clustering.R
@@ -0,0 +1,15 @@
+calculate_laplacian_matrix <- function(){
+
+}
+
+calculate_mercer_kernel(type="gauß"){
+
+}
+
+calculate_diagonal_matrix <- function(){
+
+}
+
+spectral_clustering <- function(){
+
+}
