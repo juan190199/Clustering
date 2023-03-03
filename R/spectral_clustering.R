7417f3a2d43e036e283dfa15a34051cbc55e602b
amrei <amrei@example.com>
2023-03-03 13:04:51 +0100

    Calculated diagonal matrix

--- R/spectral_clustering.R
+++ R/spectral_clustering.R
@@ -5,7 +5,7 @@ calculate_laplacian_matrix <- function(){
 calculate_mercer_kernel <- function(data, kernel_type="gauß", ...){
   # Check that Cluster Data has the right type
   source("R/utils_spectral.R")
-  check_cluster_data(data)
+  check_input_data(data)
 
   if(kernel_type == "gauß"){
     kernel_function <- function(x, y, ...) {
@@ -37,9 +37,23 @@ calculate_mercer_kernel <- function(data, kernel_type="gauß", ...){
 }
 
 
-calculate_diagonal_matrix <- function(){
+calculate_diagonal_matrix <- function(mercer_kernel){
+  # check mercer_kernel
+  source("R/utils_spectral.R")
+  check_input_data(mercer_kernel)
+  # check kernel properties
+  check_kernel_properties(mercer_kernel)
 
+  n <- nrow(mercer_kernel)
+  diagonal_matrix <- matrix(0, nrow = n, ncol = n)
+
+  for(i in seq(to=n) ){
+    diagonal_matrix[i, i] <- sum(mercer_kernel[i, ])
+  }
+  return(diagonal_matrix)
 }
+
+
 
 spectral_clustering <- function(){
 
