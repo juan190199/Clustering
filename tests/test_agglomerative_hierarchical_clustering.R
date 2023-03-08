402b56ea06054205dc2881713a963628ea4b17f0
juan190199 <juanmanueldasilvatome@gmail.com>
2023-03-08 15:56:37 +0100

    ~// Making integration tests. ToDo: Check labels

--- tests/test_agglomerative_hierarchical_clustering.R
+++ tests/test_agglomerative_hierarchical_clustering.R
@@ -3,6 +3,7 @@
 library(testthat)
 
 source("R/utils.R")
+source("R/agglomerative_hierarchical_clustering.R")
 
 init_clusters <- function(data) {
   N <- nrow(data)
@@ -72,4 +73,42 @@ test_that("merge_and_form_new_clusters merges two clusters and returns a list of
   new_clusters <- merge_and_form_new_clusters(clusters, 1, 2)
   expect_equal(length(new_clusters), length(clusters) - 1)
   expect_equal(length(new_clusters[[length(new_clusters)]]), length(clusters[[1]]) + length(clusters[[2]]))
+})
+
+############################
+##    Integration tests   ##
+############################
+
+# Define test data sets
+data1 <- data.matrix(iris[, 1:4])  # 150 observations, 4 variables
+data2 <- data.matrix(mtcars[, 1:11])  # 32 observations, 11 variables
+data3 <- data.matrix(USArrests[, 1:4])  # 50 observations, 4 variables
+
+# Define expected clustering results
+labels1 <- as.integer(iris$Species)
+labels2 <- NULL # unknown
+labels3 <- c(rep(1, 17), rep(2, 17), rep(3, 16))
+
+# Define test cases
+test_that("agglomerative_hierarchical_clustering works as expected", {
+
+  # Test case 1: data1, K=3, single linkage
+  output1 <- agglomerative_hierarchical_clustering(data1, K=3, linkage_fun="single")
+  expect_identical(output1$labels, labels1, tolerance = 0.2)
+  expect_length(output1$order, nrow(data1))
+  expect_length(length(output1$clusters), 3)
+  expect_equal(output1$method, "single")
+
+  # Test case 2: data2, K=2, complete linkage
+  output2 <- agglomerative_hierarchical_clustering(data2, K=2, linkage_fun="complete")
+  expect_length(output2$order, nrow(data2))
+  expect_length(length(output2$clusters), 2)
+  expect_equal(output2$method, "complete")
+
+  # Test case 3: data3, K=3, average linkage
+  output3 <- agglomerative_hierarchical_clustering(data3, K=3, linkage_fun="average")
+  expect_identical(output3$labels, labels3, tolerance = 0.2)
+  expect_length(output3$order, nrow(data3))
+  expect_length(length(output3$clusters), 3)
+  expect_equal(output3$method, "average")
 })
