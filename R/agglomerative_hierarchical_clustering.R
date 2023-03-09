8684a6eee62555ca9626368642bb712c478f06e6
juan190199 <juanmanueldasilvatome@gmail.com>
2023-03-09 22:13:04 +0100

    ~// Reformat

--- R/agglomerative_hierarchical_clustering.R
+++ R/agglomerative_hierarchical_clustering.R
@@ -9,84 +9,85 @@
 source("R/utils/agglomerative_hierarchical_clustering_utils.R")
 
 agglomerative_hierarchical_clustering <- function(data, K, linkage_fun, dist_method = "euclidean") {
-    N <- nrow(data)
-    measure <- get_distance_measure(linkage_fun)
-    order <- c()
-    labels <- c()
+  N <- nrow(data)
+  measure <- get_distance_measure(linkage_fun)
+  order <- c()
+  labels <- c()
 
-    sanity_checks <- function() {
-        # Check K is not bigger than the total number of data points
-        if (K > N) {
-            stop("Number of clusters asked is greater than number of data points")
+  sanity_checks <- function() {
+    # Check K is not bigger than the total number of data points
+    if (K > N) {
+      stop("Number of clusters asked is greater than number of data points")
+    }
+  }
+
+  init_clusters <- function() {
+    clusters <- list()
+    for (i in seq(N)) {
+      clusters[[i]] <- list(data[i,])
+    }
+    return(clusters)
+  }
+
+  clusters <- init_clusters()
+
+  find_closest_clusters <- function() {
+    min_dist <- Inf
+    closest_clusters <- c(NA, NA)
+
+    clusters_ids <- 1:length(clusters)
+
+    for (i in 1:(length(clusters_ids) - 1)) {
+      for (j in (i + 1):length(clusters_ids)) {
+        dist <- measure(clusters[[clusters_ids[i]]], clusters[[clusters_ids[j]]], dist_method)
+        if (dist <= min_dist) {
+          min_dist <- dist
+          closest_clusters <- c(clusters_ids[i], clusters_ids[j])
         }
+      }
+    }
+    return(closest_clusters)
+  }
+
+  merge_and_form_new_clusters <- function(ci_id, cj_id) {
+    new_clusters <- list()
+
+    for (cluster_id in 1:length(clusters)) {
+      if ((cluster_id == ci_id) | (cluster_id == cj_id)) {
+        next
+      }
+      new_clusters[[length(new_clusters) + 1]] <- clusters[[cluster_id]]
     }
 
-    init_clusters <- function() {
-        clusters <- list()
-        for(i in seq(N)){
-            clusters[[i]] <- list(data[i, ])
-        }
-        return(clusters)
+    new_clusters[[length(new_clusters) + 1]] <- c(clusters[[ci_id]], clusters[[cj_id]])
+    return(new_clusters)
+  }
+
+  run_algorithm <- function() {
+    while (length(clusters) > K) {
+      closest_clusters <- find_closest_clusters()
+      clusters <<- merge_and_form_new_clusters(closest_clusters[1], closest_clusters[2])
     }
-    clusters <- init_clusters()
+  }
 
-    find_closest_clusters <- function() {
-        min_dist <- Inf
-        closest_clusters <- c(NA, NA)
-
-        clusters_ids <- 1:length(clusters)
-
-        for (i in 1:(length(clusters_ids) - 1)) {
-            for (j in (i+1):length(clusters_ids)) {
-                dist <- measure(clusters[[clusters_ids[i]]], clusters[[clusters_ids[j]]], dist_method)
-                if (dist <= min_dist) {
-                    min_dist <- dist
-                    closest_clusters <- c(clusters_ids[i], clusters_ids[j])
-                }
-            }
-        }
-        return(closest_clusters)
+  print_ <- function() {
+    for (id in 1:length(clusters)) {
+      # cat("Cluster: ", id, "\n")
+      idx <- sapply(clusters[[id]], function(x) which(apply(data, 1, function(y) all(y == x))))
+      order <<- c(order, c(idx))
+      for (i in 1:length(clusters[[id]])) {
+        labels[idx[[i]]] <<- id
+        # cat(idx[[i]], "\t", clusters[[id]][[i]], "\n")
+      }
     }
+  }
 
-    merge_and_form_new_clusters <- function(ci_id, cj_id) {
-        new_clusters <- list()
+  sanity_checks()
+  run_algorithm()
+  print_()
 
-        for (cluster_id in 1:length(clusters)) {
-            if ((cluster_id == ci_id) | (cluster_id == cj_id)) {
-                next
-            }
-            new_clusters[[length(new_clusters) + 1]] <- clusters[[cluster_id]]
-        }
-
-        new_clusters[[length(new_clusters) + 1]] <- c(clusters[[ci_id]], clusters[[cj_id]])
-        return(new_clusters)
-    }
-
-    run_algorithm <- function() {
-        while (length(clusters) > K) {
-            closest_clusters <- find_closest_clusters()
-            clusters <<- merge_and_form_new_clusters(closest_clusters[1], closest_clusters[2])
-        }
-    }
-
-    print_ <- function() {
-        for (id in 1:length(clusters)) {
-            # cat("Cluster: ", id, "\n")
-            idx <- sapply(clusters[[id]], function(x) which(apply(data, 1, function(y) all(y == x))))
-            order <<- c(order, c(idx))
-            for (i in 1:length(clusters[[id]])) {
-                labels[idx[[i]]] <<- id
-                # cat(idx[[i]], "\t", clusters[[id]][[i]], "\n")
-            }
-        }
-    }
-
-    sanity_checks()
-    run_algorithm()
-    print_()
-
-    output <- list(order = order, clusters = clusters, method = linkage_fun, labels = labels)
-    return(output)
+  output <- list(order = order, clusters = clusters, method = linkage_fun, labels = labels)
+  return(output)
 }
 
 # X <- matrix(1:10, ncol = 2)
