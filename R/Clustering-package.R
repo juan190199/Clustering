076bff6c78caa51721175e6757363b234acaa0a1
cevinclein <hausding@live.de>
2023-03-12 19:32:29 +0100

    add package info to project

---
+++ R/Clustering-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+#' @import dplyr
+#' @import stringr
+#' @import tibble
+#' @import tidyr
+#' @import purrr
+#' @import forcats
+#' @import readr
+#' @import ggplot2
+#' @import data.table
+NULL
